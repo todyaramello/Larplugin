1618199bb53afa1c329bdff6c34a4998223b22ad
todyaramello <todyaramello@atomicmail.io>
2026-07-28 19:23:08 +0200

    fix: correct badge flag values, handle frozen users, dispatch CURRENT_USER_UPDATE, patch getUser

--- docs/index.js
+++ docs/index.js
@@ -8,9 +8,9 @@ const{ScrollView}=vendetta.metro.common.ReactNative
 const{FormInput,FormSwitchRow,FormSection}=Forms
 const h=React.createElement
 const findByStoreName=vendetta.metro.findByStoreName.bind(vendetta.metro)
-const BADGES={staff:1,partner:2,hypesquad_events:4,bughunter_1:8,bughunter_2:16,hypesquad_bravery:32,hypesquad_brilliance:64,hypesquad_balance:128,early_supporter:256,verified_developer:512,certified_moderator:262144,active_developer:4194304,http_interactions:8388608}
+const BADGES={staff:1,partner:2,hypesquad_events:4,bughunter_1:8,bughunter_2:16384,hypesquad_bravery:64,hypesquad_brilliance:128,hypesquad_balance:256,early_supporter:512,verified_developer:131072,certified_moderator:262144,active_developer:4194304,http_interactions:524288}
 function calculateFlags(badges){let flags=0;for(const[key,flag]of Object.entries(BADGES)){if(badges[key]&&flag>0)flags|=flag}return flags}
-function applyFakes(user){if(!user||!s.enabled)return;if(s.username)user.username=s.username;if(s.displayName)user.globalName=s.displayName;if(s.email)user.email=s.email;if(s.phone)user.phone=s.phone;if(s.bio)user.bio=s.bio;if(s.avatar)user.avatar=s.avatar;if(s.banner)user.banner=s.banner;if(s.avatarDecoration)user.avatarDecoration=s.avatarDecoration;const flags=calculateFlags(s.badges||{});if(flags>0||s.badges&&s.badges.nitro||s.badges&&s.badges.nitro_boost||s.badges&&s.badges.nitro_basic){user.flags=flags;user.publicFlags=flags}if(s.premiumType)user.premiumType=s.premiumType}
+function applyFakes(user){if(!user||!s.enabled)return user;if(Object.isFrozen(user)||Object.isSealed(user))user=Object.assign({},user);if(s.username)user.username=s.username;if(s.displayName)user.globalName=s.displayName;if(s.email)user.email=s.email;if(s.phone)user.phone=s.phone;if(s.bio)user.bio=s.bio;if(s.avatar)user.avatar=s.avatar;if(s.banner)user.banner=s.banner;if(s.avatarDecoration)user.avatarDecoration=s.avatarDecoration;const flags=calculateFlags(s.badges||{});if(flags>0||s.badges&&s.badges.nitro||s.badges&&s.badges.nitro_boost||s.badges&&s.badges.nitro_basic){user.flags=flags;user.publicFlags=flags}if(s.premiumType)user.premiumType=s.premiumType;return user}
 let patches=[]
 const BADGE_LIST=[{key:"staff",label:"Discord Staff"},{key:"partner",label:"Partnered Server Owner"},{key:"hypesquad_events",label:"HypeSquad Events"},{key:"bughunter_1",label:"Bug Hunter Level 1"},{key:"bughunter_2",label:"Bug Hunter Level 2"},{key:"hypesquad_bravery",label:"HypeSquad Bravery"},{key:"hypesquad_brilliance",label:"HypeSquad Brilliance"},{key:"hypesquad_balance",label:"HypeSquad Balance"},{key:"early_supporter",label:"Early Supporter"},{key:"verified_developer",label:"Early Verified Bot Developer"},{key:"certified_moderator",label:"Discord Certified Moderator"},{key:"active_developer",label:"Active Developer"},{key:"http_interactions",label:"HTTP Interactions"},{key:"nitro",label:"Nitro"},{key:"nitro_boost",label:"Server Boosting"},{key:"nitro_basic",label:"Nitro Basic"}]
 function Settings(){
@@ -46,15 +46,22 @@ return{
 onLoad:function(){
 s.enabled=s.enabled===undefined?true:s.enabled
 const UserStore=findByStoreName("UserStore")
-const user=UserStore?.getCurrentUser?.()
-if(user)applyFakes(user)
+const fakeUser=UserStore?.getCurrentUser?.()
+if(fakeUser){const mod=applyFakes(fakeUser);if(mod!==fakeUser)FluxDispatcher.dispatch({type:"CURRENT_USER_UPDATE",user:mod})}
 const origGetUser=UserStore.getCurrentUser
 UserStore.getCurrentUser=function(){
 const u=origGetUser.apply(this,arguments)
-if(u&&s.enabled)applyFakes(u)
-return u
+return u&&s.enabled?applyFakes(u):u
 }
 patches.push(function(){UserStore.getCurrentUser=origGetUser})
+const origGetUserById=UserStore.getUser
+if(origGetUserById){
+UserStore.getUser=function(id){
+const u=origGetUserById.apply(this,arguments)
+return u&&s.enabled?applyFakes(u):u
+}
+patches.push(function(){UserStore.getUser=origGetUserById})
+}
 vendetta.logger.log("[LarpPlugin] Loaded")
 },
 onUnload:function(){
